9fea8124140fe3a8e5404b35f49b5c6abb4d6e53
Zhura_is65 <muxmax969@gmail.com>
2023-08-31 09:58:52 +0300

    commit

--- Project10/main.cpp
+++ Project10/main.cpp
@@ -1,220 +1,66 @@
-//HM_ex_1_24.03.2023_
+//HM_ex_1_5.05.2023
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <algorithm>
+#include <string>
 using namespace std;
-// Клас, що представляє співробітника
-class Employee {
-public:
-    string surname;
-    string name;
-    int age;
-    Employee(const string& surname, const string& name, int age)
-        : surname(surname), name(name), age(age) {}
-};
-// Клас, що представляє базу даних співробітників
-class EmployeeDatabase
-{
-private:
-    vector<Employee> employees;
-    string filename;
-public:
-    EmployeeDatabase(const string& filename)
-        : filename(filename) {}
-    // Додавання співробітника до бази даних
-    void addEmployee(const Employee& employee)
-    {
-        employees.push_back(employee);
-    }
-    // Видалення співробітника з бази даних за прізвищем
-    void removeEmployee(const string& surname)
-    {
-        employees.erase(remove_if(employees.begin(), employees.end(),
-            [&](const Employee& employee) { return employee.surname == surname; }), employees.end());
-    }
-    // Пошук співробітників за прізвищем
-    vector<Employee> searchBySurname(const string& surname) const
-    {
-        vector<Employee> result;
-        for (const Employee& employee : employees)
-        {
-            if (employee.surname == surname)
-            {
-                result.push_back(employee);
-            }
-        }
-        return result;
-    }
-    // Пошук співробітників за віком
-    vector<Employee> searchByAge(int age) const
-    {
-        vector<Employee> result;
-        for (const Employee& employee : employees)
-        {
-            if (employee.age == age) {
-                result.push_back(employee);
-            }
-        }
-        return result;
-    }
-    // Пошук співробітників за першою літерою прізвища
-    vector<Employee> searchByFirstLetter(char letter) const
-    {
-        vector<Employee> result;
-        for (const Employee& employee : employees)
-        {
-            if (employee.surname[0] == letter)
-            {
-                result.push_back(employee);
-            }
-        }
-        return result;
-    }
-    // Завантаження списку співробітників з файлу
-    void loadFromFile()
-    {
-        ifstream file(filename);
-        if (file.is_open())
-        {
-            employees.clear();
-            string surname, name;
-            int age;
-            while (file >> surname >> name >> age)
-            {
-                employees.emplace_back(surname, name, age);
-            }
-            file.close();
-        }
-    }
-    // Збереження списку співробітників у файл
-    void saveToFile() const
-    {
-        ofstream file(filename);
-        if (file.is_open())
-        {
-            for (const Employee& employee : employees)
-            {
-                file << employee.surname << " " << employee.name << " " << employee.age << endl;
-            }
-            file.close();
-        }
-    }
-};
 int main()
 {
-    string filename = "employees.txt";
-    EmployeeDatabase database(filename);
-    // Завантаження списку співробітників з файлу
-    database.loadFromFile();
-    int choice;
-    do {
-        cout << "=== ІНФОРМАЦІЙНА СИСТЕМА \"СПІВРОБІТНИКИ\" ===" << endl;
-        cout << "1. Додати співробітника" << endl;
-        cout << "2. Видалити співробітника" << endl;
-        cout << "3. Пошук за прізвищем" << endl;
-        cout << "4. Пошук за віком" << endl;
-        cout << "5. Пошук за першою літерою прізвища" << endl;
-        cout << "6. Зберегти список співробітників у файл" << endl;
-        cout << "0. Вихід" << endl;
-        cout << "Ваш вибір: ";
-        cin >> choice;
-        switch (choice)
+    string text = "Rebrov didn't like that I called him a messiah for the national team, I think I'll survive it.";
+    string word;
+    int count = 0;
+    cout << "Enter word: ";
+    cin >> word;
+    size_t pos = text.find(word);
+    while (pos != string::npos)
+    {
+        count++;
+        pos = text.find(word, pos + 1);
+    }
+    cout << "The number of occurrences of the word '" << word << "' in the text: " << count << endl;
+    int sentence_count = 0;
+    for (size_t i = 0; i < text.length(); i++)
+    {
+        if (text[i] == '.' || text[i] == '?')
         {
-        case 1:
-        {
-            string surname, name;
-            int ag
-                cout << "Введіть прізвище: ";
-            cin >> surname;
-            cout << "Введіть ім'я: ";
-            cin >> name;
-            cout << "Введіть вік: ";
-            cin >> age;
-            database.addEmployee(Employee(surname, name, age));
-            cout << "Співробітник успішно доданий!" << endl;
-            break;
+            sentence_count++;
         }
-        case 2:
+    }
+    cout << "Number of sentences in the text: " << sentence_count << endl;
+    int dot_count = 0;
+    int comma_count = 0;
+    for (size_t i = 0; i < text.length(); i++)
+    {
+        if (text[i] == '.')
         {
-            string surname;
-            cout << "Введіть прізвище співробітника, якого бажаєте видалити: ";
-            cin >> surname;
-            database.removeEmployee(surname);
-            cout << "Співробітник успішно видалений!" << endl;
-            break;
+            dot_count++;
         }
-        case 3:
+        else if (text[i] == ',')
         {
-            string surname;
-            cout << "Введіть прізвище для пошуку: ";
-            cin >> surname;
-            vector<Employee> result = database.searchBySurname(surname);
-            if (result.empty()) {
-                cout << "Співробітників з таким прізвищем не знайдено." << endl;
-            }
-            else
+            comma_count++;
+        }
+    }
+    cout << "The number of points in the text: " << dot_count << endl;
+    cout << "The number of commas in the text: " << comma_count << endl;
+    string reversed_text;
+    for (int i = text.length() - 1; i >= 0; i--)
+    {
+        reversed_text += text[i];
+    }
+    cout << "Inverted text: " << reversed_text << endl;
+    string reversed_sentences;
+    string sentence;
+    for (size_t i = 0; i < text.length(); i++)
+    {
+        sentence += text[i];
+        if (text[i] == '.' || text[i] == '?')
+        {
+            for (int j = sentence.length() - 2; j >= 0; j--)
             {
-                cout << "Результати пошуку:" << endl;
-                for (const Employee& employee : result) {
-                    cout << "Прізвище: " << employee.surname << ", Ім'я: " << employee.name << ", Вік: " << employee.age << endl;
-                }
+                reversed_sentences += sentence[j];
             }
-            break;
+            reversed_sentences += sentence[sentence.length() - 1];
+            sentence = "";
         }
-        case 4:
-        {
-            int age;
-            cout << "Введіть вік для пошуку: ";
-            cin >> age;
-            vector<Employee> result = database.searchByAge(age);
-            if (result.empty())
-            {
-                cout << "Співробітників з таким віком не знайдено." << endl;
-            }
-            else
-            {
-                cout << "Результати пошуку:" << endl;
-                for (const Employee& employee : result)
-                {
-                    cout << "Прізвище: " << employee.surname << ", Ім'я: " << employee.name << ", Вік: " << employee.age << endl;
-                }
-            }
-            break;
-        }
-        case 5:
-        {
-            char letter;
-            cout << "Введіть літеру для пошуку: ";
-            cin >> letter;
-            vector<Employee> result = database.searchByFirstLetter(letter);
-            if (result.empty())
-            {
-                cout << "Співробітників з прізвищами, що починаються на задану літеру, не знайдено." << endl;
-            }
-            else
-            {
-                cout << "Результати пошуку:" << endl;
-                for (const Employee& employee : result)
-                {
-                    cout << "Прізвище: " << employee.surname << ", Ім'я: " << employee.name << ", Вік: " << employee.age << endl;
-                }
-            }
-            break;
-        }
-        case 6:
-        {
-            database.saveToFile();
-            cout << "Список співробітників успішно збережений у файл!" << endl;
-            break;
-        }
-        case 0:
-            break;
-        default:
-            cout << "Невірний вибір. Спробуйте ще раз." << endl;
-            break;
-        }
-        cout << endl;
-    } while (choice != 0);
+    }
+    cout << "Inverted sentences in the text: " << reversed_sentences << endl;
     return 0;
 }
